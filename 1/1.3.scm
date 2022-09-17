9c004fb2dc84f6461ac66fdc217474b19c223b62
Abel Mulugeta <abelmulugeta2014@gmail.com>
2022-09-17 19:41:24 +0300

    1.32,1.33

--- 1/1.3.scm
+++ 1/1.3.scm
@@ -9,6 +9,11 @@
     (+ x dx))
   (* (sum f (+ a (/ dx 2.0)) add-dx b) dx))
 
+(define (identity n)
+  n)
+(define (add-one n)
+  (+ n 1))
+
 ;;; 1.29
 
 ;; h/3 [(y0+yn) + 4(y1+y3+y5+...+y(n-1)) + 2(y2+y4+y6+...+yn)]
@@ -39,16 +44,37 @@
   (iter a 1))
 
 (define (factorial-product n)
-  (define (identity n)
-    n)
-  (define (next n)
-    (+ n 1))
 
-  (product-iter identity 1 next n))
+  (product-iter identity 1 add-one n))
 
 (define (wallis-pi n)
   (define (term k)
     (if (even? k) (/ (+ k 2) (+ k 1)) (/ (+ k 1) (+ k 2))))
-  (define (next n)
-    (+ n 1))
-  (* 4.0 (product-iter term 1 next n)))
+  (* 4.0 (product-iter term 1 add-one n)))
+
+;;; 1.32
+(define (accumulate combiner null-value term a next b)
+  (if (> a b) null-value (combiner (term a) (accumulate combiner null-value term (next a) next b))))
+
+(define (accumulate-iter combiner null-value term a next b)
+  (define (iter a result)
+    (if (> a b) result (iter (next a) (combiner (term a) result))))
+  (iter a null-value))
+
+;;; 1.33
+(define (filtered-accumulate combiner null-value term a next b filter)
+  (if (> a b)
+      null-value
+      (if (filter a)
+          (combiner (term a) (filtered-accumulate combiner null-value term (next a) next b filter))
+          (filtered-accumulate combiner null-value term (next a) next b filter))))
+
+(define (filtered-accumulate-iter combiner null-value term a next b filter)
+  (define (iter a result)
+    (if (> a b) result (iter (next a) (if (filter a) (combiner (term a) result) result))))
+  (iter a null-value))
+
+; (define (sum-square-prime a b)
+;   (define (combiner a b)
+;     (+ a b))
+;   (filtered-accumulate-iter combiner 0 identity a add-one b prime?))
