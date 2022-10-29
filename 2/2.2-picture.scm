9c4ee0b5f91bfbcbb14a2f4d6a73072e09a73fe7
Abel Mulugeta <abelmulugeta2014@gmail.com>
2022-10-29 22:26:19 +0300

    2.45,2.46,2.47

--- 2/2.2-picture.scm
+++ 2/2.2-picture.scm
@@ -26,3 +26,48 @@
   (if (= n 0)
       painter
       (let ([smaller (up-split painter (- n 1))]) (below painter (beside smaller smaller)))))
+
+;;; 2.45
+
+(define (split proc1 proc2)
+  (lambda (painter n)
+    (if (= n 0)
+        painter
+        (let ([smaller ((split proc1 proc2) painter (- n 1))])
+          (proc1 painter (proc2 smaller smaller))))))
+
+;;; 2.46
+
+(define (make-vect x y)
+  (cons x y))
+(define (xcor-vect vect)
+  (car vect))
+(define (ycor-vect vect)
+  (cdr vect))
+
+(define (add-vect v1 v2)
+  (cons (cons (+ (xcor-vect v1) (xcor-vect v2)) (+ (ycor-vect v1) (ycor-vect v2)))))
+(define (sub-vect v1 v2)
+  (cons (cons (- (xcor-vect v1) (xcor-vect v2)) (- (ycor-vect v1) (ycor-vect v2)))))
+(define (scale-vect v s)
+  (cons (* (xcor-vect v) s) (* (ycor-vect v) s)))
+
+;;; 2.47
+(define (make-frame origin edge1 edge2)
+  (list origin edge1 edge2))
+(define (origin-frame frame)
+  (car frame))
+(define (edge1-frame frame)
+  (cadr frame))
+(define (edge2-frame frame)
+  (caddr frame))
+
+(define (make-frame-cons origin edge1 edge2)
+  (cons origin (cons edge1 edge2)))
+(define (origin-frame-cons frame)
+  (origin-frame frame))
+(define (edge1-frame-cons frame)
+  (edge1-frame frame))
+(define (edge2-frame-cons frame)
+  (cddr frame))
+
